295431151f329110622740ab0554a48b50417313
Hardik <hardikagrawal9521@gmail.com>
2024-05-08 07:08:31 +0530

    latest

--- feature.py
+++ feature.py
@@ -18,7 +18,7 @@ class FeatureExtraction:
     features = []
     def __init__(self,url):
         self.features = []
-        self.url = url
+        self.url =url
         self.domain = ""
         self.whois_response = ""
         self.urlparse = ""
@@ -469,12 +469,10 @@ class FeatureExtraction:
             return -1
 
     # 30. StatsReport
-            # ...
-
+    def StatsReport(self):
+        try:
             url_match = re.search(
-                'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly', self.url)
-            url_match = re.search(
-        'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly', url)
+                'at\.ua|usa\.cc|baltazarpresentes\.com\.br|pe\.hu|esy\.es|hol\.es|sweddy\.com|myjino\.ru|96\.lt|ow\.ly',url)
             ip_address = socket.gethostbyname(self.domain)
             ip_match = re.search('146\.112\.61\.108|213\.174\.157\.151|121\.50\.168\.88|192\.185\.217\.116|78\.46\.211\.158|181\.174\.165\.13|46\.242\.145\.103|121\.50\.168\.40|83\.125\.22\.219|46\.242\.145\.98|'
                                 '107\.151\.148\.44|107\.151\.148\.107|64\.70\.19\.203|199\.184\.144\.27|107\.151\.148\.108|107\.151\.148\.109|119\.28\.52\.61|54\.83\.43\.69|52\.69\.166\.231|216\.58\.192\.225|'
